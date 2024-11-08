d0c923a5662e715466d310c8ab1699aba5a1b857
govher <govhertm@gmail.com>
2024-11-08 11:34:22 +0100

    efter opgave1

--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -1,4 +1,13 @@
 package storage;
 
+import model.Bestilling;
+import model.Forestilling;
+import model.Kunde;
+
+import java.util.ArrayList;
+
 public class Storage {
+    private static ArrayList<Forestilling> forestillinger = new ArrayList<>();
+    private static ArrayList<Bestilling> bestillinger = new ArrayList<>();
+    private static ArrayList<Kunde> kunder = new ArrayList<>();
 }
